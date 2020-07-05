71828a0fb71fbc7e7034b67e5f2019a263d33b4e
hobbyluv <princewillchime43@gmail.com>
2020-07-05 16:29:11 +0100

    added a background color to the register screen

--- src/Register/Styles.components/registered_Styles.js
+++ src/Register/Styles.components/registered_Styles.js
@@ -5,6 +5,7 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#66b2b2',
   },
   touchableOpacity: {
     width: 200,
