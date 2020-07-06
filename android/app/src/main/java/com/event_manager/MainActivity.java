7bed972a523399f1c77be4f45144598f7bd92c54
hobbyluv <princewillchime43@gmail.com>
2020-07-06 11:47:48 +0100

    changed the android app icon and made some adjust to the android files

--- android/app/src/main/java/com/event_manager/MainActivity.java
+++ android/app/src/main/java/com/event_manager/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "learn-react_native";
+    return "schedule app";
   }
 }
